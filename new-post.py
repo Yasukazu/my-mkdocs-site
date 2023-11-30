bb6d127eb207fe74679219f5da144dd71b127a51
Yasukazu Makino <yasukazu@makyno.jp>
2023-12-01 00:10:30 +0900

    new post script

---
+++ new-post.py
@@ -0,0 +1,16 @@
+import sys, datetime
+if len(sys.argv) <= 1:
+	print('Needs slug')
+	sys.exit(1)
+node = sys.argv[1]
+filespec = f'docs/posts/{node}.md'
+with open(filespec, 'w') as output:
+	# print header
+	print('---', file=output)
+	title = node.replace('-', ' ')
+	print('title: ' + title, file=output)
+	iso=datetime.datetime.now().isoformat()
+	today=iso[0 : iso.index('T')]
+	print("date: " + today, file=output)
+	print('---', file=output)
+print(filespec + ' is created.')
