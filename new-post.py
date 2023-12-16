bf537ec6de928a33fce56b6e69ce37efd158bfc7
Yasukazu Makino <mkn@yskz.dev>
2023-12-16 21:32:21 +0900

    slugify rstrip hypen

--- new-post.py
+++ new-post.py
@@ -20,8 +20,7 @@ def slugify(cc: str):
 	output = io.StringIO()
 	for n in nodes:
 		print(''.join([c if c.isalnum() else '-' for c in n]), file=output, end='_')
-	return output.getvalue().rstrip('_')
-breakpoint()
+	return output.getvalue().rstrip('_').rstrip('-')
 args = [n for n in sys.argv[1:]]
 title = ' '.join(args)
 print('title: ' + title, file=output)
@@ -33,7 +32,6 @@ header = output.getvalue()
 slugs = [slugify(n) for n in args]
 node = '_'.join(slugs)
 filespec = os.path.join(posts_dir, node + '.md')
-breakpoint()
-with open(filespec, 'w') as output:
-	print(header, output=output)
+with open(filespec, 'w') as out:
+	print(header, file=out)
 print(filespec + ' is created.')
