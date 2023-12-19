80f8496084c64d64621104bc5eb953433fc910ca
Yasukazu Makino <mkn@yskz.dev>
2023-12-19 10:04:46 +0900

    slugify yaml datetime.date.today()

--- new-post.py
+++ new-post.py
@@ -1,5 +1,8 @@
+# - generate new post: 1st argument is title, later ones are tags
+# - title is slugified into the filename of the post
 import sys, datetime, os, io, yaml
-from pathvalidate import sanitize_filename
+from slugify import slugify
+# from pathvalidate import sanitize_filename
 # from jinja2 import Template
 
 posts_dir = os.path.join('docs', 'posts')
@@ -7,12 +10,12 @@ if not os.path.isdir(posts_dir):
 	sys.exit(f"{posts_dir} does not exist!")
 
 if len(sys.argv) <= 1:
-	print('Needs slug')
+	print('New post generator. Needs at least 1 argument as the title, later ones become tags.')
 	sys.exit(1)
-node = '-'.join([n for n in sys.argv[1:]])
+#node = '-'.join([n for n in sys.argv[1:]])
+tags = sys.argv[2:]
 # output = io.StringIO()
 
-filespec = os.path.join(posts_dir, node + '.md')
 # with open(filespec, 'w') as output:
 	# print header
 '''
@@ -24,31 +27,15 @@ date: {{ date }}
 '''
 HR = '---'
 
-def slugify(cc: str):
-	cc = cc.strip().rstrip() # remove leading and trailing spaces
-	# nodes = cc.split(' ')
-	# output = io.StringIO()
-	# for n in nodes:
-		# print(''.join([c.lower() if c.isalnum() else '-' for c in n]), file=output, end='_')
-	return  sanitize_filename(cc).lower().replace(' ', '-')
-	# return output.getvalue().rstrip('_').rstrip('-')
 args = sys.argv[1:]
-title = ';'.join(args)
+title = args[0]
 # print('title: ' + title, file=output)
-iso=datetime.datetime.now().isoformat()
-date=iso[0 : iso.index('T')]
-meta = {'title': title, 'date': date}
-front_mat = yaml.dump(meta)
+today = datetime.date.today()
+meta = {'title': title, 'date': today, 'tags': tags}
+front_mat = yaml.dump(meta).replace('\n-', '\n -')
 # print(HR, file=output) print(front_mat, file=output) print(HR, file=output)
 header = '\n'.join([HR, front_mat, HR]) # output.getvalue()
-slugs = [slugify(n) for n in args]
-node = '.'.join(slugs)
-filespec = os.path.join(posts_dir, node + '.md')
-if os.path.exists(filespec):
-	print("It's already exists:")	
-	print(filespec)	
-	sys.exit(2)
-
+filespec = os.path.join(posts_dir, slugify(title) + '.md')
 with open(filespec, 'w') as out:
 	print(header, file=out)
 
@@ -59,3 +46,13 @@ else:
 	print('Following file is not created!')
 	print(filespec)
 	sys.exit(3)
+"""
+def slugify(cc: str):
+	cc = cc.strip().rstrip() # remove leading and trailing spaces
+	# nodes = cc.split(' ')
+	# output = io.StringIO()
+	# for n in nodes:
+		# print(''.join([c.lower() if c.isalnum() else '-' for c in n]), file=output, end='_')
+	return  sanitize_filename(cc).lower().replace(' ', '-')
+	# return output.getvalue().rstrip('_').rstrip('-')
+"""
